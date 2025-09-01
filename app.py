4b184d9c2d89204cc7398c040bfc7088a881423d
shannaka <shanna.brown16@gmail.com>
2025-08-31 23:40:52 -0700

    Add files via upload
    Signed-off-by: shannaka <shanna.brown16@gmail.com>

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Shanna-kay! Your Flask app is running 🎉"
+    return "Hello from GKE!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
